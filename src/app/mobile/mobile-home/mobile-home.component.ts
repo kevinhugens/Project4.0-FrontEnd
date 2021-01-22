637c5f39cbc29fcf992e6c493b02f6485e994657
kevin <kevin@KEVIN-MBP>
2021-01-22 10:44:07 +0100

    qrscanning

--- src/app/mobile/mobile-home/mobile-home.component.ts
+++ src/app/mobile/mobile-home/mobile-home.component.ts
@@ -1,20 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UserService } from '../../shared/services/user.service'
+import { ZXingScannerComponent } from '@zxing/ngx-scanner';
+import { Result } from '@zxing/library';
+
 @Component({
   selector: 'app-mobile-home',
   templateUrl: './mobile-home.component.html',
   styleUrls: ['./mobile-home.component.scss']
 })
 export class MobileHomeComponent implements OnInit {
+  @ViewChild('scanner')
+  scanner: ZXingScannerComponent;
+
+  hasDevices: boolean;
+  hasPermission: boolean;
+  qrResultString: string;
+  qrResult: Result;
+
+  availableDevices: MediaDeviceInfo[];
+  currentDevice: MediaDeviceInfo;
 
   constructor(private _userService: UserService) { }
 
   ngOnInit(): void {
+    this.scanner.camerasFound.subscribe((devices: MediaDeviceInfo[]) => {
+      this.hasDevices = true;
+      this.availableDevices = devices;
+
+      // selects the devices's back camera by default
+      for (const device of devices) {
+          if (/back|rear|environment/gi.test(device.label)) {
+              this.currentDevice = device;
+              break;
+          }
+      }
+    });
+
+    this.scanner.camerasNotFound.subscribe(() => this.hasDevices = false);
+    this.scanner.scanComplete.subscribe((result: Result) => this.qrResult = result);
+    this.scanner.permissionResponse.subscribe((perm: boolean) => this.hasPermission = perm);
   }
 
   onCodeResult(resultString: String) {
-    this._userService.getUserByToken(resultString).subscribe((data) => {
-      console.log(data);
-    });
+    //this._userService.getUserByToken(resultString).subscribe((data) => {
+      //console.log(data);
+   // });
+   alert(resultString);
   }
 }
